f9c18f9ffdc1dab3ac930d9ad43572f95313a5e3
foop <60478352+weiyi-m@users.noreply.github.com>
2021-03-16 04:46:23 +0800

    most recent version of ddbot.js

--- ddbot.js
+++ ddbot.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./ddconfig.json');
+const { token, prefix } = require('./ddconfig.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -14,33 +14,29 @@ for (const file of commandFiles) {
 
 const cooldowns = new Discord.Collection();
 
-// startup message
+// if all goes well initially
 client.once('ready', () => {
 	console.log('Alright, I\'m in the system, chief. Go ahead!');
 });
 
 client.on('message', message => {
-	
-	// return if message doesn't start with prefix OR message is sent by a bot
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
-	
-	// get command from the commands folder
+
 	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-	
-	// if there's no commandName found in the commands folder, return
+
 	if (!client.commands.has(commandName)) return;
-	
-	// cooldowns (not sure if it properly functions currently
-	// TODO: make this work
+
 	if (!cooldowns.has(command.name)) {
 		cooldowns.set(command.name, new Discord.Collection());
 	}
-	
+
 	const now = Date.now();
 	const timestamps = cooldowns.get(command.name);
+	
+	// cooldown variable not really implemented
 	const cooldownAmount = (command.cooldown || 2) * 1000;
 
 	if (timestamps.has(message.author.id)) {
@@ -54,19 +50,16 @@ client.on('message', message => {
 		timestamps.set(message.author.id, now);
 		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 	}
-	
-	// execute command, return error message if present
+
 	try {
 		command.execute(message, args, client);
 	} catch (error) {
 		console.error(error);
-		message.reply('something went wrong, code must be broken or something.');
+		message.reply('there was an error trying to execute that command!');
 	}
 });
 
-// the entirety of this chunk is still in progress
-// it's to send a welcome message whenever a member joins the guild
-// something went wrong with the member.createdAt so i'm excluding this bit out so that i can work on it
+// this was meant to be a welcome message, but had some trouble printing out the account creation date
 /*
 client.on('guildMemberAdd', member => {
 	const guild = client.guilds.cache.get('756902642768150538');
@@ -79,7 +72,6 @@ client.on('guildMemberAdd', member => {
 })
 */
 
-// basic error logging, nothing much to see
 client.on('shardError', error => {
     console.error('A websocket connection encountered an error:', error);
 })
@@ -88,5 +80,4 @@ process.on('unhandledRejection', error => {
     console.error('Unhandled promise rejection:', error);
 })
 
-// if all goes well,
 client.login(token);
